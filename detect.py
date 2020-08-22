9a08c01febd72178f25c328413cad42e0ab96a85
Kiran Ghimire <47425314+Cimihan123@users.noreply.github.com>
2020-08-22 14:45:52 +0545

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,11 +1,12 @@
 import dns.zone
 import pydig
 import dns.resolver
+import socket
 import argparse
 import time
 from core.utils import url_list
 from core.colors import red ,end, yellow,green,minus
-
+import subprocess
 
 
 parser = argparse.ArgumentParser(description='Help is Here')
@@ -23,12 +24,20 @@ urls = url_list(target,wordlist)
 def DnsResolve(urls):
 
     for target in urls:
-            
+
         try:
             with open('ns.txt','a+') as file:
                 dns_resolver = pydig.query(target,'NS')
                 file = [file.write(str(dns)+'\n') for dns in dns_resolver]
-        except:
+
+                if file:
+                  print(f"[{minus}] %s NameServer Found for {red}{target}{end}"%(green))
+                else:
+
+                 print(f"[{minus}] %s No NameServer Found for {yellow}{target}{end}"%(red))
+        except subprocess.CalledProcessError:
+            print(f"[{minus}] %s No NameServer Found for {yellow}{target}{end}"%(red))
+        except :
             print(f"[{minus}] %s No NameServer Found for {yellow}{target}{end}"%(red))
 
 
@@ -37,7 +46,7 @@ def DnsResolve(urls):
 lines = set()
 outfile = open('ns.txt', "w")
 for line in open('ns.txt', "r"):
-    if line not in lines: 
+    if line not in lines:
         outfile.write(line)
         lines.add(line)
 outfile.close()
@@ -54,7 +63,7 @@ def zoneXFR(target):
                 print('No NameServers Present in ns.txt file')
             else:
                 try :
-                        query = dns.zone.from_xfr(dns.query.xfr(server ,target,timeout=3,lifetime=2))
+                        query = dns.zone.from_xfr(dns.query.xfr(server ,target,timeout=5,lifetime=6))
                         node = query.nodes.keys()
                         for i in node:
                             axfr = query[i].to_text(i)
@@ -73,11 +82,12 @@ def zoneXFR(target):
                     print(f"[{minus}] %s No vulnerabilty Discovered for  {yellow}{server} at {target}{end}"%(red))
                 except  dns.exception.SyntaxError :
                     print(f"[{minus}] %s No vulnerabilty Discovered for  {yellow}{server} at {target}{end}"%(red))
-
+                except socket.gaierror:
+                    print(f"[{minus}] %s No vulnerabilty Discovered for  {yellow}{server} at {target}{end}"%(red))
             time.sleep(0.1)
 
 #saving nameserver
 DnsResolve(urls)
 
 #zonetransfer
-zoneXFR((target))
+zoneXFR(target)
